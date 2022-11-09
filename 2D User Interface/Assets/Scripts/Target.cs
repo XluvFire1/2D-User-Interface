a175a7166a88adf4354572f0398201524a899a20
Jacob Zambrano <jacobzambrano858@riversideunified.org>
2022-11-09 11:02:34 -0800

    11/9/22

---
+++ 2D User Interface/Assets/Scripts/Target.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int PointValue = 1;
+    public float MinSpeed = 10;
+    public float MaxSpeed = 20;
+    public float MaxTorque = 10;
+
+    private Rigidbody2D _targetRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetRb = GetComponent<Rigidbody2D>();
+        _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
+        _targetRb.AddTorque(RandomizeTorque());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private float RandomizeForce()
+    {
+        return Random.Range(MinSpeed, MaxSpeed);
+    }
+    private float RandomizeTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
+    }
+}
