77fd86cdc9a36e6dbb31aee59d281ca501d46e6b
Jacob Zambrano <jacobzambrano858@riversideunified.org>
2022-11-10 11:03:30 -0800

    11/10/22

--- 2D User Interface/Assets/Scripts/Target.cs
+++ 2D User Interface/Assets/Scripts/Target.cs
@@ -10,11 +10,14 @@ public class Target : MonoBehaviour
     public float MaxTorque = 10;
 
     private Rigidbody2D _targetRb;
+    private GameManager _gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         _targetRb = GetComponent<Rigidbody2D>();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
         _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
         _targetRb.AddTorque(RandomizeTorque());
     }
@@ -32,5 +35,21 @@ public class Target : MonoBehaviour
     private float RandomizeTorque()
     {
         return Random.Range(-MaxTorque, MaxTorque);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+
+        if(!other.gameObject.CompareTag("Bad"))
+        {
+            //Debug.Log("Game Over");
+            _gameManager.IsGameActive = false;
+        }
     }
 }
